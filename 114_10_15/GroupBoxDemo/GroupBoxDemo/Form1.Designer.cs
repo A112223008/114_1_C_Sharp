58ea906a6177e3ecb68185bbd00db8137acca543
A112223008 <a112223008@mail.shu.edu.tw>
2025-10-15 13:02:23 +0800

    114_10_15

---
+++ 114_10_15/GroupBoxDemo/GroupBoxDemo/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace GroupBoxDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupbox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupbox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupbox1
+            // 
+            this.groupbox1.Controls.Add(this.textBox1);
+            this.groupbox1.Controls.Add(this.button1);
+            this.groupbox1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.groupbox1.Location = new System.Drawing.Point(298, 116);
+            this.groupbox1.Name = "groupbox1";
+            this.groupbox1.Size = new System.Drawing.Size(661, 526);
+            this.groupbox1.TabIndex = 0;
+            this.groupbox1.TabStop = false;
+            this.groupbox1.Text = "GBDemo";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(280, 133);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 36);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1436, 914);
+            this.Controls.Add(this.groupbox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupbox1.ResumeLayout(false);
+            this.groupbox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupbox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 114_10_15/Tutorial 4-2/Payroll with Overtime/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll_with_Overtime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // 設定按鈕的 Text 為繁體中文            
+        }
+
+        // 當使用者點擊「計算薪資」按鈕時執行
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            // 定義基本工時與加班倍數
+            const decimal BASEHOURS = 40m; // 正常工時上限
+            const decimal OT_MULTIPLIER = 1.5m; // 加班時薪倍數
+
+            decimal hoursWorked;     // 儲存輸入的工時
+            decimal hourlyPayRate;   // 儲存輸入的時薪
+            decimal grossPay;       // 儲存計算後的薪資總額
+
+
+            try
+            {
+                // 取得使用者輸入的工時與時薪，並轉換為 decimal 型別
+                hoursWorked = decimal.Parse(hoursWorkedTextBox.Text);
+                hourlyPayRate = decimal.Parse(hourlyPayRateTextBox.Text);
+                // 判斷是否有加班
+                if (hoursWorked <= BASEHOURS)
+                {
+                    // 沒有加班，直接計算薪資
+                    grossPay = hoursWorked * hourlyPayRate;
+                }
+                else
+                {
+                    // 有加班，先計算基本工時薪資，再加上加班薪資
+                    grossPay = (BASEHOURS * hourlyPayRate) +
+                               ((hoursWorked - BASEHOURS) * hourlyPayRate * OT_MULTIPLIER);
+                }
+                // 顯示計算結果於標籤上
+                grossPayLabel.Text = grossPay.ToString("c");
+            }
+            catch (Exception ex)
+            {
+                // 若輸入格式錯誤，顯示錯誤訊息
+                MessageBox.Show("請輸入正確的數值格式！\n" + ex.Message, "輸入錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // 當使用者點擊「清除」按鈕時執行
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // 清空所有輸入欄位與結果標籤
+            hoursWorkedTextBox.Text = "";
+            hourlyPayRateTextBox.Text = "";
+            grossPayLabel.Text = "";
+
+            // 將游標焦點移回工時輸入欄位
+            hoursWorkedTextBox.Focus();
+        }
+
+        // 當使用者點擊「離開」按鈕時執行
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉視窗
+            this.Close();
+        }
+    }
+}
