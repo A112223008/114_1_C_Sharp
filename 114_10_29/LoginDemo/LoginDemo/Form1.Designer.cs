e141c5d312c2921ff8048babc2deb66f1f7fa2c5
A112223008 <a112223008@mail.shu.edu.tw>
2025-10-29 13:01:14 +0800

    114_10_29

---
+++ 114_10_29/LoginDemo/LoginDemo/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace LoginDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        // 新增的控制項欄位
+        private System.Windows.Forms.Label labelAccount;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxAccount;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.Label labelResult;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelAccount = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxAccount = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelAccount
+            // 
+            this.labelAccount.AutoSize = true;
+            this.labelAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelAccount.Location = new System.Drawing.Point(69, 50);
+            this.labelAccount.Name = "labelAccount";
+            this.labelAccount.Size = new System.Drawing.Size(134, 52);
+            this.labelAccount.TabIndex = 0;
+            this.labelAccount.Text = "帳號 :";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelPassword.Location = new System.Drawing.Point(69, 133);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(134, 52);
+            this.labelPassword.TabIndex = 1;
+            this.labelPassword.Text = "密碼 :";
+            // 
+            // textBoxAccount
+            // 
+            this.textBoxAccount.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBoxAccount.Location = new System.Drawing.Point(229, 45);
+            this.textBoxAccount.Name = "textBoxAccount";
+            this.textBoxAccount.Size = new System.Drawing.Size(549, 57);
+            this.textBoxAccount.TabIndex = 2;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBoxPassword.Location = new System.Drawing.Point(229, 130);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(549, 57);
+            this.textBoxPassword.TabIndex = 3;
+            this.textBoxPassword.UseSystemPasswordChar = true;
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonCheck.Location = new System.Drawing.Point(348, 256);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(150, 60);
+            this.buttonCheck.TabIndex = 4;
+            this.buttonCheck.Text = "檢查";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelResult.Location = new System.Drawing.Point(78, 347);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(700, 222);
+            this.labelResult.TabIndex = 5;
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 626);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonCheck);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxAccount);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelAccount);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ 114_10_29/MultipleConditionsDemo/MultipleConditionsDemo/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipleConditionsDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJudge_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int score = int.Parse(tbxScore.Text);
+                string grade = "";
+
+                #region //--一個一個列條件--//
+                //    if (score >= 90)
+                //    {
+                //        grade = "A";
+                //    }
+                //    else if (score >= 80) //Score < 90是隱含條件
+                //    {
+                //        grade = "B";
+                //    }
+                //    else if (score >= 70) //Score < 80是隱含條件
+                //    {
+                //        grade = "C";
+                //    }
+                //    else if (score >= 60) //Score < 70是隱含條件
+                //    {
+                //        grade = "D";
+                //    }
+                //    else
+                //    {
+                //        grade = "F";
+                //    }
+                //    lblGrade.Text = grade;
+                //}
+                //catch (Exception ex)
+                //{
+                //    MessageBox.Show("請輸入有效的成績數值。\n" + ex.Message);
+                //}
+                #endregion
+
+                //--巢狀條件寫法--//                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+                if (score >= 60)
+                {
+                    if (score >= 70)
+                    {
+                        if (score >= 80)
+                        {
+                            if (score >= 90)
+                            {
+                                grade = "A";
+                            }
+                            else
+                            {
+                                grade = "B";
+                            }
+                        }
+                        else
+                        {
+                            grade = "C";
+                        }
+                    }
+                    else
+                    {
+                        grade = "D";
+                    }
+                }
+                else
+                {
+                    grade = "F";
+                }
+                lblGrade.Text = grade;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("請輸入有效的成績數值。\n" + ex.Message);
+            }
+        }
+    }
+}
